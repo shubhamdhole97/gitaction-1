e7116e1b294ebbfd93ab5c9b06749b34dc7acec5
shubhamdhole97 <shubhamdhole97@gmail.com>
2025-08-05 14:44:35 +0530

    1

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+resource "google_compute_instance" "ubuntu_vm" {
+  name         = "ubuntu-instance"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  tags = ["ssh"]
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${var.ssh_pub_key}"
+  }
+}
+
+output "vm_ip" {
+  value = google_compute_instance.ubuntu_vm.network_interface[0].access_config[0].nat_ip
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+  default     ="absolute-hub-460713-v0"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "ssh_user" {
+  description = "SSH user for the instance"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "ssh_pub_key" {
+  description = "Public SSH key content"
+  type        = string
+}
